051af99f974e4aabf5ab350ad28aeb4c2d19adbd
Luiz Guilherme Bandeira <arkanael@gmail.com>
2017-09-13 09:38:14 -0300

    Add project files.

---
+++ OpenStock.Entity/Base/Cadastro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenStock.Entity.Base
+{
+    /// <summary>
+    /// Essa classe deverá ser herdada por outras classes que necessite registrar qualquer tipo de cadastro e edição.
+    /// </summary>
+    public class Cadastro
+    {
+        public string CadastradoPor { get; set; }
+        public string EditadoPor { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataEdicao { get; set; }
+    }
+}
---
+++ OpenStock.Entity/Usuario.cs
@@ -0,0 +1,37 @@
+﻿using OpenStock.Entity.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenStock.Entity
+{
+    public class Usuario: Cadastro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public string Foto { get; set; }
+
+        public Usuario()
+        {
+
+        }
+
+        public Usuario(int id, string nome, string login, string senha, string foto)
+        {
+            Id = id;
+            Nome = nome;
+            Login = login;
+            Senha = senha;
+            Foto = foto;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome} Data de Cadastro: {DataCadastro}";
+        }
+    }
+}
